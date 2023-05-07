656b489c75fd765eedcdc3644232e27932448a71
List <61047602+namest504@users.noreply.github.com>
2023-05-07 20:18:47 +0900

    init

---
+++ src/test/java/com/list/WChatProject/WChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.WChatProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WChatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
