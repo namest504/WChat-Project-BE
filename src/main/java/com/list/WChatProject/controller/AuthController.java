0e1f6cecae45aa57e18a3f00e3c1c5c10093c62e
List <61047602+namest504@users.noreply.github.com>
2023-05-08 02:43:24 +0900

    fix : 카카로 로그인 API

--- src/main/java/com/list/WChatProject/controller/AuthController.java
+++ src/main/java/com/list/WChatProject/controller/AuthController.java
@@ -39,11 +39,15 @@ public class AuthController {
         KakaoAccountInformationRequestResponse kakaoAccountInformationRequestResponse = kakaoAPIService
                 .getKakaoAccountInformation(kakaoAccountToken);
         LOGGER.info("kakaoAccountInformationRequestResponse.getKakao_account().getEmail() : {}", kakaoAccountInformationRequestResponse.getKakao_account().getEmail());
+
+        LOGGER.info("kakaoAccountInformationRequestResponse.getKakao_account().getEmail() : {}", kakaoAccountInformationRequestResponse.getKakao_account().getEmail());
         LOGGER.info("kakaoAccountInformationRequestResponse.getKakao_account().getProfile() : {}",kakaoAccountInformationRequestResponse.getKakao_account().getProfile().getNickname());
         LOGGER.info("kakaoAccountInformationRequestResponse.getId() : {}",kakaoAccountInformationRequestResponse.getId());
 
         String kakaoId = kakaoAccountInformationRequestResponse.getId();
-        authService.register(new RegisterRequestDto(kakaoAccountInformationRequestResponse.getKakao_account().getEmail(), null, AccountType.KAKAO));
+        if (!authService.isRegisterdKakao(kakaoId)) {
+            authService.register(new RegisterRequestDto(kakaoAccountInformationRequestResponse.getKakao_account().getEmail(), null, AccountType.KAKAO));
+        }
 
         Long uid = authService.getUidFromKakaoAccount(kakaoId);
         String accessToken = jwtService.createAccessToken(uid);
