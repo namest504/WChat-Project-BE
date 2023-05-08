d2cdea220e3655ec6ce203c6496450dae7e1eb96
List <61047602+namest504@users.noreply.github.com>
2023-05-08 15:44:33 +0900

    feat : JPA 쿼리

--- src/main/java/com/list/WChatProject/repository/ChatRoomRepository.java
+++ src/main/java/com/list/WChatProject/repository/ChatRoomRepository.java
@@ -3,6 +3,10 @@ package com.list.WChatProject.repository;
 import com.list.WChatProject.chat.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
     ChatRoom findByRoomId(String roomId);
+    Optional<List<ChatRoom>> findChatRoomsByRoomNameContaining(String roomName);
 }
