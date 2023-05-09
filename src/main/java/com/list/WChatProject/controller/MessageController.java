82ee1934a1f7bedbfb1af574b1c4c754c826ba44
List <61047602+namest504@users.noreply.github.com>
2023-05-09 17:15:54 +0900

    feat : bucket4j 메세지 전송량 제한 기능 구현

--- src/main/java/com/list/WChatProject/controller/MessageController.java
+++ src/main/java/com/list/WChatProject/controller/MessageController.java
@@ -2,13 +2,20 @@ package com.list.WChatProject.controller;
 
 import com.list.WChatProject.chat.ChatMessage;
 import com.list.WChatProject.chat.MessageType;
+import com.list.WChatProject.exception.CustomException;
 import com.list.WChatProject.service.ChatService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 
 @RestController
@@ -18,18 +25,35 @@ public class MessageController {
     private final SimpMessageSendingOperations sendingOperations;
     private final ChatService chatService;
     private final Logger LOGGER = LoggerFactory.getLogger(MessageController.class);
+
+    //Refill.intervally toekn = 2, 1회충전시 2개의 토큰을 충전
+    //Duration.ofSeconds = 1, 1초마다 토큰을 충전
+    Refill refill = Refill.intervally(2, Duration.ofSeconds(1));
+
+    //Bandwidth capacity = Bucket의 총 크기는 3
+    Bandwidth limit = Bandwidth.classic(3,refill);
+
+    //총크기가 3이며 1초마다 2개의 Token을 충전하는 Bucket 생성
+    Bucket bucket = Bucket.builder()
+            .addLimit(limit)
+            .build();
+    //bucket.tryConsume(1)
     @MessageMapping("/chat/message")
     public void enter(ChatMessage message) {
-        if (MessageType.ENTER.equals(message.getType())) {
-            message.setMessage(message.getSender() + "님이 입장하였습니다.");
+        if (bucket.tryConsume(1)) {
+            if (MessageType.ENTER.equals(message.getType())) {
+                message.setMessage(message.getSender() + "님이 입장하였습니다.");
+            }
+            if (MessageType.EXIT.equals(message.getType())) {
+                message.setMessage(message.getSender() + "님이 퇴장하였습니다.");
+            }
             chatService.countPeopleChatRoom(message.getRoomId(), message.getType().getValue());
+            sendingOperations.convertAndSend("/topic/chat/room/" + message.getRoomId(), message);
+            LOGGER.info("[/topic/chat/room/{}]으로 ", message.getRoomId());
+            LOGGER.info("[{}] 가 [{}]를 보냈습니다.", message.getSender(), message.getMessage());
+        } else {
+            throw new CustomException(HttpStatus.TOO_MANY_REQUESTS, "메세지 전송량이 너무 많습니다.");
         }
-        if (MessageType.EXIT.equals(message.getType())) {
-            message.setMessage(message.getSender() + "님이 퇴장하였습니다.");
-            chatService.countPeopleChatRoom(message.getRoomId(), message.getType().getValue());
-        }
-        sendingOperations.convertAndSend("/topic/chat/room/" + message.getRoomId(), message);
-        LOGGER.info("[/topic/chat/room/{}]으로 ", message.getRoomId());
-        LOGGER.info("[{}] 가 [{}]를 보냈습니다.", message.getSender(), message.getMessage());
+
     }
 }
