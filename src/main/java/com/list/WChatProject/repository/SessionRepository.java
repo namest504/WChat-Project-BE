d2a518fcec60d0ef10df29e74e58ca7bba1053f3
List <61047602+namest504@users.noreply.github.com>
2023-05-10 02:35:17 +0900

    feat : Session Repository 추가

---
+++ src/main/java/com/list/WChatProject/repository/SessionRepository.java
@@ -0,0 +1,12 @@
+package com.list.WChatProject.repository;
+
+import com.list.WChatProject.entity.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    Optional<Session> findSessionByMemberId(Long memberId);
+}
