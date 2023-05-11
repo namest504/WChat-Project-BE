d49e1ba5d4e6f703180ccf38639e4c1f23433592
List <61047602+namest504@users.noreply.github.com>
2023-05-12 03:37:42 +0900

    test: 대화 상대 찾기 로직 테스트

--- src/main/java/com/list/WChatProject/repository/MemberRepository.java
+++ src/main/java/com/list/WChatProject/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import com.list.WChatProject.entity.AccountType;
 import com.list.WChatProject.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,6 +18,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Boolean existsByNickName(String nickName);
     Optional<Member> findByUserIdAndAccountType(String userId, AccountType accountType);
 
-    @Query("select m.nickName from Member m where m.id in (select s.member.id from Session s where s.chatRoom.roomId = :roomid)")
-    List<Member> findMemberInRoom(String roomId);
+    @Query("select m.nickName from Member m where m.id in (select s.member.id from Session s where s.chatRoom.roomId = :roomId)")
+    List<Member> findMemberInRoom(@Param("roomId") String roomId);
 }
